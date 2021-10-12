f16168acd66b5de7c9f0574e7426d7f3b5618cbb
AntZapot <antoniozapot@hotmail.com>
2021-10-12 11:42:08 -0500

    second commit

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body'))
 app.use(cors())
+app.use(express.static('build'))
 
 app.get('/info', (request , response) => {
     const date = new Date();
